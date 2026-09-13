4875017ed95cd6468ad2d06a56e10c3248379c3a
Psix-anp <77142468+Psix-anp@users.noreply.github.com>
2026-09-13 11:37:09 +0300

    feat: expose TTS voice service

--- custom_components/matrix_extended/const.py
+++ custom_components/matrix_extended/const.py
@@ -24,6 +24,7 @@ DEFAULT_INCOMING_MEDIA_RETENTION_DAYS: Final = 7
 DEFAULT_INCOMING_MEDIA_MAX_MB: Final = 256
 
 SERVICE_SEND: Final = "send"
+SERVICE_SEND_VOICE: Final = "send_voice"
 SERVICE_SEND_LOCATION: Final = "send_location"
 SERVICE_REPLY: Final = "reply"
 SERVICE_REACT: Final = "react"
